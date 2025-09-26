06bafb2f5dc402ec0b548249bea791a2d0ebeaed
youssef shokr <usf254@gmail.com>
2025-09-26 18:12:57 +0300

    Add files via upload

---
+++ unit of measurement conversion.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const double cm_in_m = 100.0;
+const double g_in_kg = 1000.0;
+const double sec_in_min = 60.0;
+const double min_in_hr = 60.0;
+
+double cm_to_m(double cm) {
+    return cm / cm_in_m;
+}
+
+double m_to_cm(double m) {
+    return m * cm_in_m;
+}
+
+double g_to_kg(double g) {
+    return g / g_in_kg;
+}
+
+double kg_to_g(double kg) {
+    return kg * g_in_kg;
+}
+
+double sec_to_min(double sec) {
+    return sec / sec_in_min;
+}
+
+double min_to_sec(double min) {
+    return min * sec_in_min;
+}
+
+double min_to_hr(double min) {
+    return min / min_in_hr;
+}
+
+double hr_to_min(double hr) {
+    return hr * min_in_hr;
+}
+
+int main() {
+    int opt;
+    double val;
+
+    cout << "c++ unit converter (length / mass / time)" << endl;
+
+    cout << "select the conversion operation:" << endl;
+    cout << "1. centimeters to meters" << endl;
+    cout << "2. meters to centimeters" << endl;
+    cout << "3. grams to kilograms" << endl;
+    cout << "4. kilograms to grams" << endl;
+    cout << "5. seconds to minutes" << endl;
+    cout << "6. minutes to seconds" << endl;
+    cout << "7. minutes to hours" << endl;
+    cout << "8. hours to minutes" << endl;
+    cout << "9. exit" << endl;
+
+    cout << "enter your choice (1 to 9): ";
+
+    if (!(cin >> opt)) {
+        cerr << "\ninvalid input. please enter a number." << endl;
+        return 1;
+    }
+
+    cout << fixed << setprecision(2);
+
+    if (opt == 1) {
+        cout << "\nenter the value in centimeters: ";
+        cin >> val;
+        cout << "value in meters: " << cm_to_m(val) << " m" << endl;
+
+    } else if (opt == 2) {
+        cout << "enter the value in meters: " <<endl;
+        cin >> val;
+        cout << "value in centimeters: " << m_to_cm(val) << " cm" << endl;
+
+    } else if (opt == 3) {
+        cout << "\nenter the value in grams: ";
+        cin >> val;
+        cout << "value in kilograms: " << g_to_kg(val) << " kg" << endl;
+
+    } else if (opt == 4) {
+        cout << "\nenter the value in kilograms: ";
+        cin >> val;
+        cout << "value in grams: " << kg_to_g(val) << " g" << endl;
+
+    } else if (opt == 5) {
+        cout << "\nenter the value in seconds: ";
+        cin >> val;
+        cout << "value in minutes: " << sec_to_min(val) << " min" << endl;
+
+    } else if (opt == 6) {
+        cout << "\nenter the value in minutes: ";
+        cin >> val;
+        cout << "value in seconds: " << min_to_sec(val) << " sec" << endl;
+
+    } else if (opt == 7) {
+        cout << "\nenter the value in minutes: ";
+        cin >> val;
+        cout << "value in hours: " << min_to_hr(val) << " hr" << endl;
+
+    } else if (opt == 8) {
+        cout << "\nenter the value in hours: ";
+        cin >> val;
+        cout << "value in minutes: " << hr_to_min(val) << " min" << endl;
+
+    } else if (opt == 9) {
+        cout << "\nthank you for using the converter. goodbye!" << endl;
+
+    } else {
+        cout << "\ninvalid choice. please enter a number between 1 and 9." << endl;
+    }
+
+    return 0;
+}
